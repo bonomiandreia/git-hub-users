90f93d84f40b11094ba45280cdaa4dd2841d94e1
bonomiandreia <bonomiandreia@gmail.com>
2021-11-02 00:51:08 -0300

    users

--- list-users/src/app/users/users.module.ts
+++ list-users/src/app/users/users.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './page/users/users.component';
 import { TableUsersComponent } from './component/table-users/table-users.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRoutingModule } from './users.module.routing';
 import { MatTableModule } from '@angular/material/table'  
 import { HttpClientModule } from '@angular/common/http';
